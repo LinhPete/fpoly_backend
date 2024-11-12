19dc1705c2ae9a1665f0c056c05c9d1fe59bcf08
Linh Pete <peterdamlinh1215@gmail.com>
2024-11-12 19:53:03 +0700

    Lab3,4 done

---
+++ PS38327_NGUYENDAMHOANGLINH_ASM/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Layout from './components/Layout.vue';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+</script>
+
+<template>
+  <Layout/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ PS38327_NGUYENDAMHOANGLINH_ASM/src/components/Layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+    import LayoutNav from './LayoutNav.vue';
+</script>
+
+<template>
+<div class="row">
+    <div class="col-lg-3 col-md-4" style="position: sticky; top: 0;">
+        <LayoutNav/>
+    </div>
+    <div class="col-lg-9 col-md-8">
+
+    </div>
+</div>
+      
+</template>
+
+<style scoped>
+
+</style>
